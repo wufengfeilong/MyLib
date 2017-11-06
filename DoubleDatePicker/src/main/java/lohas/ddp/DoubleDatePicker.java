4fd053e8f37f58569f2f34740f0b103afb592dd4
wufengfeilong <wufengfeilong@163.com>
2017-11-06 10:55:52 +0800

    Initial commit

---
+++ DoubleDatePicker/src/main/java/lohas/ddp/DoubleDatePicker.java
@@ -0,0 +1,19 @@
+package lohas.ddp;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DoubleDatePicker extends View{
+    public DoubleDatePicker(Context context) {
+        super(context);
+    }
+
+    public DoubleDatePicker(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DoubleDatePicker(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
